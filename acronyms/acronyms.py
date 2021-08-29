cfbcde5de035715ae1ace638d518dafb963ce5f9
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2021-08-29 16:59:18 +0200

    Fixed me a little bug

--- acronyms/acronyms.py
+++ acronyms/acronyms.py
@@ -7,7 +7,7 @@ import requests
 def main():
 
     root = Tk()
-    requests.get('http://textfiles.com/humor/acronym.txt')
+    resp = requests.get('http://textfiles.com/humor/acronym.txt')
     acronyms = resp.text.splitlines()[5:]
     label_text = random.choice(resp.text.splitlines()[5:])
     a = Label(root, text = label_text)
